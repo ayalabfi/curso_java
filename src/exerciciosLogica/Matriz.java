30475f6cc1a880baef43abbe6d2ecd90d7667c46
Ayala <ayalaborbafilho@gmail.com>
2025-12-22 16:53:29 -0300

    Encerramento da "Seção 10: Comportamento da memória, arrays, listas".

--- src/exerciciosLogica/Matriz.java
+++ src/exerciciosLogica/Matriz.java
@@ -18,7 +18,7 @@ public class Matriz {
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++){
-                System.out.printf("Digite o valor na coluna %d° da linha %d°", j, i);
+                System.out.printf("Digite o valor na %d° coluna da %d° linha: ", j, i);
                 mat[i][j] = sc.nextInt();
             }
         }
@@ -29,20 +29,20 @@ public class Matriz {
         for (int i = 0; i < mat.length; i++){
             for (int j = 0; j < mat[i].length; j++) {
                 if (x == mat[i][j]){
-                    int esquerda = mat[i][j-1];
                     System.out.println("Posição: " + i + ", " + j);
-                    if(){
-
+                    if(j > 0){
+                        System.out.println("Esquerda: " + mat[i][j - 1]);
                     }
-                    if{
-
+                    if(j < mat[i].length - 1){
+                        System.out.println("Direita: " + mat[i][j + 1]);
                     }
-                    if{
-
+                    if(i > 0){
+                        System.out.println("Acima: " + mat[i - 1][j]);
                     }
-                    if{
-
+                    if(i < mat.length - 1){
+                        System.out.println("Abaixo: " + mat[i + 1][j]);
                     }
+                    System.out.println();
                 }
             }
         }
