7c0e536c70a68ae3d16a7f6d6a6e6efab3428c20
Ayala <ayalaborbafilho@gmail.com>
2026-01-20 17:17:38 -0300

    Introdução a: "Seção 13: Herança e polimorfismo".

---
+++ src/orientacaoAObjetos/entidades/ClasseExtensao.java
@@ -0,0 +1,36 @@
+package orientacaoAObjetos.entidades;
+
+// Significa que a "ClasseHerança" tem todos os comportamentos (atributos e métodos) da "ClasseHerdada"
+public class ClasseExtensao extends ClasseHeranca {
+
+    private Double limiteEmprestimo;
+
+    public ClasseExtensao(){
+        // Caso alguma lógica seja implementada no construtor da classe base, ela também será implementada na classe derivada
+        super();
+    }
+
+    // Para o IntelliJ, basta selecionar primeiro o construtor da classe base e depois o construtor da classe derivada.
+    // Para o Eclipse, é necessário ao invés de escolher "Generate Constructor", escolher a opção "Generate Constructor
+    // using fields".
+    public ClasseExtensao(Integer numero, String titular, Double balance, Double limiteEmprestimo) {
+        // Chama o construtor da classe base (ClasseHerdada)
+        super(numero, titular, balance);
+        this.limiteEmprestimo = limiteEmprestimo;
+    }
+
+    public Double getLimiteEmprestimo() {
+        return limiteEmprestimo;
+    }
+
+    public void setLimiteEmprestimo(Double limiteEmprestimo) {
+        this.limiteEmprestimo = limiteEmprestimo;
+    }
+
+    public void emprestimo(Double quantia){
+        if (limiteEmprestimo >= quantia) {
+            // só é possível referenciar o saldo pois ele está como protected, e não como private
+            saldo += quantia - 10;
+        }
+    }
+}
---
+++ src/orientacaoAObjetos/entidades/ClasseHeranca.java
@@ -0,0 +1,46 @@
+package orientacaoAObjetos.entidades;
+
+public class ClasseHeranca {
+
+    private Integer numero;
+    private String titular;
+    // protected faz com que o atributo não seja publico, mas possa ser acessado pela classe derivada
+    protected Double saldo;
+
+    public ClasseHeranca(){
+    }
+
+    public ClasseHeranca(Integer numero, String titular, Double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar (double quantia){
+        saldo -= quantia;
+    }
+
+    public void depositar (double quantia){
+        saldo += quantia;
+    }
+}
